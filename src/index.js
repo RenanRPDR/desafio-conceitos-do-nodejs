dd4819252b12193ffcead2d463c282fe70840c4c
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2022-05-31 23:48:59 -0300

    Primeiro desafio concluido.

--- src/index.js
+++ src/index.js
@@ -69,15 +69,43 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
   const { id } = request.params;
 
-  const todo = user.todos.find(todo => todo.id = id)
+  const todo = user.todos.find(todo => todo.id = id);
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found."})
+  }
+
+  todo.title = title;
+  todo.deadline = deadline;
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id = id);
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found."})
+  }
+
+  todo.done = true;
+
+  return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: "Todo not found."})
+  }
+
+ user.todos.splice(todoIndex, 1);
+
+ return response.status(204).send();
 });
 
 module.exports = app;
